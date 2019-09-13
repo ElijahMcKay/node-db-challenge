7d5a78dd4d4fb5388598833826d33216ac65d1c9
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-09-13 10:40:44 -0500

    migration completed

---
+++ data/migrations/20190913101443_projects.js
@@ -0,0 +1,57 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('projects', tbl => {
+      tbl.increments(); 
+      tbl.string('project_name')
+        .notNullable()
+      tbl.string('project_description')
+      tbl.boolean('project_complete')
+        .notNullable()
+    .createTable('resources', tbl => {  // resources table 
+        tbl.increments()
+        tbl.string('resource_name')
+            .notNullable()
+        tbl.string('resource_description')
+            .notNullable(); 
+    })
+    .createTable('project_resources', tbl => {
+        tbl.integer('project_id')
+            .unsigned() 
+            .notNullable()
+            .references('id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+        tbl.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('resources')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
+    .createTable('tasks', tbl => {
+        tbl.increments()
+        tbl.string('task_description')
+            .notNullable()
+        tbl.string('notes')
+        tbl.boolean('task_complete')
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })  
+        
+
+  })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('tasks')
+        .dropTableIfExists('project_resources')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects')
+};
