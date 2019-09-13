2629cb391a18084d590fbccacdb4a1ef283c8e67
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-09-13 10:42:41 -0500

    knexfile configured

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,18 @@ module.exports = {
       filename: './data/projects.db3'
     },
     useNullAsDefault: true,
-
+    migrations: {
+      directory: './data/migrations'  //you may also want to add this migrations directory 
+    },
+    seeds: {
+      directory: './data/seeds'  //you may also want to add this migrations directory 
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+          //runs after a connection is made to the sqlite engine
+          conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    }
   },
 
 }
